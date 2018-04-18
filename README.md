5950e94b98b347e44ca578225fa081eac185d90b
吴浩 <530414935@qq.com>
2018-04-18 20:57:33 +0800

    解决git冲突

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# spring-boot-oauth
+
+
+# spring-boot-security-oauth2
+
+该项目是一个基于spring boot 1.4.5
+整合spring security oauth2 
+
+使用jwt方式存储的一个认证服务和资源服务分离的例子demo
+1.auth-server 统一权限认证服务器，负责校验登录，获取token等
+2.rsc-server api接口服务器,访问接口时带上token作为参数
+
+版本不能升级到1.5.X，存在版本兼容问题
+
+
---
+++ auth-server/src/main/java/com/alex/repository/AccountRepository.java
@@ -0,0 +1,22 @@
+package com.alex.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alex.entity.Account;
+
+/**
+ * 处理数据到mongodb数据库
+ * 
+ * @author alex
+ *
+ */
+@Repository
+public interface AccountRepository extends JpaRepository<Account, String> {
+	/**
+	 * 参照spring data jpa 可以根据方法名称查询出数据无需实现
+	 * @param name
+	 * @return
+	 */
+	Account findByName(String name);
+}
