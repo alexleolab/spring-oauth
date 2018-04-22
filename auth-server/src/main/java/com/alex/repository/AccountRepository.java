5af46317e97e75fd8723609148c423b07958b99a
吴浩 <johndoe@example.com>
2018-04-22 20:08:57 +0800

    切换分支异常解决

--- auth-server/src/main/java/com/alex/repository/AccountRepository.java
+++ auth-server/src/main/java/com/alex/repository/AccountRepository.java
@@ -13,10 +13,12 @@ import com.alex.entity.Account;
  */
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
+	
 	/**
 	 * 参照spring data jpa 可以根据方法名称查询出数据无需实现
 	 * @param name
 	 * @return
 	 */
 	Account findByName(String name);
+	
 }
